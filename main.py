4303b6c0171384d42364d90066af53aed2de896a
Gabriel <gbizzotto@yahoo.com>
2018-11-07 00:13:35 -0200

    Made as a telegram bot if ran with command line arg 'bot'.

--- main.py
+++ main.py
@@ -1,97 +1,56 @@
 
-import random
+import time
+import requests
+import json
 import sys
 import unicodedata
 import datetime
-from py2casefold import casefold
 
+import telepot
+from telepot.loop import MessageLoop
+from telepot.namedtuple import InlineQueryResultArticle, InputTextMessageContent
+
+import study
 import kodule
 import kb
+import telegrambot
 
-def normalize_caseless(text):
-    return unicodedata.normalize("NFKD", casefold(text))
+class Input:
+    def __init__(self):
+        self.value = ''
+input = Input()
 
-def print_lesson(l, depth):
-    print '  '*depth + 'Lesson', l.title, ':'
-    for m in l.initial_material:
-        print '  '*(depth+1) + 'Material:', m
-    for tr in l.translations:
-        print '  '*(depth+1) + 'Tr: ', unicode(tr)
-    for t,tr in l.translations_by_tag.iteritems():
-        print '  '*(depth+1) +'Tag:', t, tr
+sessions = {}
+bot = None
 
-def print_kodule(k, depth):
-    print '  '*depth + ('Course' if k.is_kourse else 'Module'), k.title, ':'
-    for d in k.dependencies:
-        print_kodule(d, depth+1)
-    for l in k.kessons:
-        print_lesson(l, depth+1)
+def handle(msg):
+    global sessions
+    global bot
+    global input
+    print msg
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    if chat_id not in sessions:
+        sessions[chat_id] = study.choose_kourse(input)
+    if content_type == 'text':
+        input.value = msg['text']
+        bot.sendMessage(chat_id, sessions[chat_id].next())
 
-def revise(knowledge_base):
-    kbis = knowledge_base.get_kbis_to_revise()
-    if len(kbis) == 0:
-        return
-    while kbis[0].next_revision_time <= datetime.datetime.now():
-        kbi = kbis[0]
-        if kbi.translation.secret is False:
-            question = random.choice(kbi.translation.natives)
-            answers = knowledge_base.answers(question)
-            tries = 0
-            while True:
-                tentative = raw_input('(g for giveup) ' + question + ' > ').decode(sys.stdin.encoding)
-                tries += 1
-                if tentative == 'g':
-                    print 'Translations:', ', '.join(answers)
-                    continue
-                if normalize_caseless(tentative) not in [normalize_caseless(t) for t in answers]:
-                    print('Wrong answer')
-                    continue
-                break
-        del kbis[0]
-        if tries == 1:
-            kbi.got_it_right_on_1st_try()
-        else:
-            kbi.got_it_right_eventually()
-        kbis.add(kbi)
-        # print('See you around', kbi.next_revision_time)
+def console_main():
+    global input
+    for x in study.choose_kourse(input):
+        input.value = raw_input(x + ' > ').decode(sys.stdin.encoding)
 
-def study(kodule, root_kodule, knowledge_base):
-    for dep in kodule.dependencies:
-        study(dep, root_kodule, knowledge_base)
-
-    while revise(knowledge_base):
-        pass
-
-    for kesson in kodule.kessons:
-        if knowledge_base.has_kesson(kesson):
-            continue
-        knowledge_base.add_kesson(kesson)
-        print('Adding module "' + kodule.title + '", lesson "' + kesson.title + '"')
-        print('Initial material:')
-        for im in kesson.initial_material:
-            print '  ' + im
-
-    while revise(knowledge_base):
-        pass
-
-def main():
-    kodules = kodule.load_all('./kodules')
-    kourses = [k for (n,k) in kodules.iteritems() if k.is_kourse]
-    while(True):
-        i=1
-        for k in kourses:
-            print str(i)+'.', k.title
-            i += 1
-        raw_choice = raw_input("Which kourse? ")
-        if not raw_choice.isdigit():
-            continue
-        selected_item = int(raw_choice) - 1
-        if selected_item < 0 or selected_item >= len(kourses):
-            continue
-        break
-    knowledge_base = kb.KnowledgeBase() # TODO load from file/DB
-    study(kourses[selected_item], kourses[selected_item], knowledge_base)
-    print "You're all set for now. Please come back around", str(knowledge_base.get_next_revision_time().time())[:5]
+def bot_main():
+    global bot
+    bot = telepot.Bot(telegrambot.key)
+    # MessageLoop(bot, {'chat': handle}).run_as_thread()
+    MessageLoop(bot, handle).run_as_thread()
+    print ('Listening ...')
+    while 1:
+        time.sleep(10)
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) > 1 and sys.argv[1] == 'bot':
+        bot_main()
+    else:
+        console_main()
---
+++ study.py
@@ -0,0 +1,102 @@
+
+import time
+import random
+import sys
+import unicodedata
+import datetime
+
+from py2casefold import casefold
+
+import kodule
+import kb
+
+def normalize_caseless(text):
+    return unicodedata.normalize("NFKD", casefold(text))
+
+# def print_lesson(l, depth):
+#     print '  '*depth + 'Lesson', l.title, ':'
+#     for m in l.initial_material:
+#         print '  '*(depth+1) + 'Material:', m
+#     for tr in l.translations:
+#         print '  '*(depth+1) + 'Tr: ', unicode(tr)
+#     for t,tr in l.translations_by_tag.iteritems():
+#         print '  '*(depth+1) +'Tag:', t, tr
+
+# def print_kodule(k, depth):
+#     print '  '*depth + ('Course' if k.is_kourse else 'Module'), k.title, ':'
+#     for d in k.dependencies:
+#         print_kodule(d, depth+1)
+#     for l in k.kessons:
+#         print_lesson(l, depth+1)
+
+def revise(input, knowledge_base):
+    kbis = knowledge_base.get_kbis_to_revise()
+    if len(kbis) == 0:
+        return
+    while kbis[0].next_revision_time <= datetime.datetime.now():
+        kbi = kbis[0]
+        if kbi.translation.secret is False:
+            question = random.choice(kbi.translation.natives)
+            answers = knowledge_base.answers(question)
+            tries = 0
+            hint = ''
+            while True:
+                yield hint + '(g for giveup)\n' + question
+                tentative = input.value
+                tries += 1
+                if tentative == 'g':
+                    hint = 'Translations: ' + ', '.join(answers) + '\n'
+                    continue
+                if normalize_caseless(tentative) not in [normalize_caseless(t) for t in answers]:
+                    hint = 'Wrong answer\n'
+                    continue
+                break
+        del kbis[0]
+        if tries == 1:
+            kbi.got_it_right_on_1st_try()
+        else:
+            kbi.got_it_right_eventually()
+        kbis.add(kbi)
+        # print('See you around', kbi.next_revision_time)
+
+def study(input, kodule, root_kodule, knowledge_base):
+    for dep in kodule.dependencies:
+        for x in study(input, dep, root_kodule, knowledge_base):
+            yield x
+
+    for x in revise(input, knowledge_base):
+        yield x
+
+    for kesson in kodule.kessons:
+        if knowledge_base.has_kesson(kesson):
+            continue
+        knowledge_base.add_kesson(kesson)
+        output = 'Adding module "' + kodule.title + '", lesson "' + kesson.title + '"\n'
+        output += 'Initial material:\n'
+        for im in kesson.initial_material:
+            output += '  ' + im + '\n'
+
+    for x in revise(input, knowledge_base):
+        yield output + x
+        output = ''
+
+def choose_kourse(input):
+    kodules = kodule.load_all('./kodules')
+    kourses = [k for (n,k) in kodules.iteritems() if k.is_kourse]
+    while(True):
+        i=1
+        output = ''
+        for k in kourses:
+            output = output + str(i)+'. ' + k.title + '\n'
+            i += 1
+        yield output + "Which kourse?"
+        if not input.value.isdigit():
+            continue
+        selected_item = int(input.value) - 1
+        if selected_item < 0 or selected_item >= len(kourses):
+            continue
+        break
+    knowledge_base = kb.KnowledgeBase() # TODO load from file/DB
+    for x in study(input, kourses[selected_item], kourses[selected_item], knowledge_base):
+        yield x
+    yield "You're all set for now. Please come back around " + str(knowledge_base.get_next_revision_time().time())[:5]
